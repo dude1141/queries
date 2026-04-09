6765ccd22b8d59ae3a55a82a67b799b55d940102
Mithelesh_1141 <48873398+dude1141@users.noreply.github.com>
2026-04-08 23:48:20 -0400

    Add SQL script for new and repeat customers
    Create SQL script to identify new and repeat customers based on order data.

---
+++ 2026sqls/findnew_and_repeat.sql
@@ -0,0 +1,17 @@
+find new and repate customers
+
+
+input
+# order_id	customer_id	order_date	order_amount
+    1	        100	        2022-01-01	2000
+    2	        200        	2022-01-01	2500
+    3	        300	        2022-01-01	2100
+    4	        100	        2022-01-02	2000
+    5        	400	        2022-01-02	2200
+    6	        500	        2022-01-02	2700
+    7	        100        	2022-01-03	3000
+    8	        400	        2022-01-03	1000
+    9	        600	        2022-01-03	3000
+
+
+#you need to first get order_date and minus it with ,using case when
