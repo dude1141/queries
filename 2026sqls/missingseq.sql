89416824ada04e87ac33b0b118cec52817193925
Mithelesh_1141 <48873398+dude1141@users.noreply.github.com>
2026-04-10 14:22:09 -0400

    Update missingseq.sql

--- 2026sqls/missingseq.sql
+++ 2026sqls/missingseq.sql
@@ -13,11 +13,31 @@ from numbers)
 
 
 # prevs	num	nexts
-	      1	    2
+	null  1	    2
     1	  2	    3
     2	  3     5
     3	  5	    6
     5	  6	    8
-    6  	8	    9
-    8	  9	  10
-        9	  10	
+    6  	  8	    9
+    8	  9	   10
+    9	  10  null
+
+
+with cte1 as (
+select
+lag(num) over ( order by num) as prevs,
+num,
+lead(num) over (order by num ) as nexts
+from numbers)
+select num+1  as missingfrom, nexts-1 as missingto
+from cte1 where nexts-num >1;
+
+we need to substract where nexts-num>1 so obvioulsy  i will be nums of 3 and 6 >1
+so we need to get the num+1 and nexts-1
+
+# missingfrom	missingto
+		4			4
+		7			7
+
+
+
