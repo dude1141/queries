e0ff907fcea8b9bdb9ff3afe8725e954e5e1d1c9
Mithelesh_1141 <48873398+dude1141@users.noreply.github.com>
2026-04-13 13:44:46 -0400

    Update sourcedestination.sql

--- 2026sqls/sourcedestination.sql
+++ 2026sqls/sourcedestination.sql
@@ -20,9 +20,36 @@ Mumbai  ,    Delhi,      400
 Chennai,      Pune,      400
 
 
-select distinct a.source_city ,b.source_city,a.distance  ,
+/*select distinct a.source_city ,b.source_city,a.distance  ,
 case when a.source_city=b.destination_city then 1 else 0 end as 'source flag',
 case when a.source_city !=b.destination_city then 1 else 0 end as 'target flag'
-From routes a join routes b on a.source_city= b.destination_city;
+From routes a join routes b on a.source_city= b.destination_city; */
+
+on self join if you want to restrict rows , you can compare id for example here
+
+
+with cte as (
+select *,row_number() over () as id from routes
+)
+select a.source_city as asrccity, a.destination_city as bdestcity
+, b.source_city as bsrccity , b.destination_city as bdestination,
+a.distance as adist ,b.distance as bdist
+ From cte a join cte b
+ on a.source_city= b.destination_city
+
+
+# asrccity	bdestcity	bsrccity	bdestination	adist	bdist
+Bangalore	Hyderabad	Hyderabad	Bangalore	400	400
+Hyderabad	Bangalore	Bangalore	Hyderabad	400	400
+Mumbai	        Delhi	Delhi	    Mumbai    	400	400
+Delhi	        Mumbai	Mumbai	    Delhi	    400	400
+Chennai	           Pune	Pune	    Chennai	    400	400
+Pune	        Chennai	        Chennai	Pune	400	400
+.
+
+# asrccity	bdestcity	bsrccity	bdestination	adist	bdist
+Bangalore	Hyderabad	Hyderabad	Bangalore	    400	    400
+Mumbai	    Delhi    	Delhi	    Mumbai    	    400	    400
+Chennai    	Pune	    Pune	    Chennai        	400	    400
 
 
